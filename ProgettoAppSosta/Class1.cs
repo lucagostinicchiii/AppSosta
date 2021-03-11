772f1e2f8ea73e91bd6af7afe7fd6442655ee80d
gosti <gosti@LAPTOP-RSCNUK9K>
2021-03-11 13:04:48 +0100

    verifica tariffe parcheggio

--- ProgettoAppSosta/Class1.cs
+++ ProgettoAppSosta/Class1.cs
@@ -2,7 +2,32 @@
 
 namespace ProgettoAppSosta
 {
-    public class Class1
+    public class Gestione
     {
+        public static double tariffaA(int ore, double costoTariffa)
+        {
+            double costoTot = 15;
+            return costoTot;
+        }
+        public static double tariffaB(int ore, double costoTariffa)
+        {
+            double costoTot = ore * costoTariffa;
+            return costoTot;
+        }
+        public static double tariffaC(int ore,double costoTariffa)
+        {
+            double costoTot = 0;
+            if (ore==1)
+            {
+                costoTot = 0;
+            }
+            else
+            {
+                costoTot = (ore - 1) * costoTariffa;
+            }
+            return costoTot;
+        }
+           
+        
     }
 }
--- TestAppSosta/UnitTest1.cs
+++ TestAppSosta/UnitTest1.cs
@@ -1,13 +1,37 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProgettoAppSosta;
 
 namespace TestAppSosta
 {
     [TestClass]
-    public class UnitTest1
+    public class TestVerifica
     {
         [TestMethod]
-        public void TestMethod1()
+        public void tariffa1()
         {
+            int ore = 3;
+            double tariffa = 15;
+            double risposta_aspettata = 15;
+            double risposta_effettiva = Gestione.tariffaA(ore, tariffa);
+            Assert.AreEqual(risposta_effettiva, risposta_aspettata);
+        }
+        [TestMethod]
+        public void tariffaB()
+        {
+            int ore = 3;
+            double tariffa = 1;
+            double risposta_aspettata = 3;
+            double risposta_effettiva = Gestione.tariffaB(ore, tariffa);
+            Assert.AreEqual(risposta_aspettata, risposta_effettiva);
+        }
+        [TestMethod]
+        public void tariffaC()
+        {
+            int ore = 2;
+            double tariffa = 1.20;
+            double risposta_aspettata = 1.20;
+            double risposta_effettiva = Gestione.tariffaC(ore, tariffa);
+            Assert.AreEqual(risposta_aspettata, risposta_effettiva);
         }
     }
 }
